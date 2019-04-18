fda21d017eb351f3aca016b8880701f285c78a9b
Isai Lopez <ilopezro@ucsc.edu>
2019-04-17 22:03:51 -0700

    circle.js file created and will draw triangles; circle script also added

---
+++ asg1/src/graphics/geometry/shapes/circle.js
@@ -0,0 +1,39 @@
+/**
+ * Specifies a circle. A subclass of geometry.
+ *
+ * @author Lucas N. Ferreira
+ * @this {Circle}
+ */
+class Circle extends Geometry {
+    /**
+     * Constructor for Circle.
+     *
+     * @constructor
+     * @param {Shader} shader Shading object used to shade geometry
+     * @returns {Circle} Triangle created
+     */
+    constructor(shader, g_points, rColor, gColor, bColor, size) {
+        super(shader);
+  
+        this.vertices = this.generateTriangleVertices(g_points, rColor, gColor, bColor, size);
+        this.faces = {0: this.vertices};
+  
+        // CALL THIS AT THE END OF ANY SHAPE CONSTRUCTOR
+        this.interleaveVertices();
+    }
+  
+    generateTriangleVertices(g_points, rColor, gColor, bColor, size) {
+        var vertices = []
+        
+        var vertex1 = new Vertex(g_points[0], g_points[1]+Number(size), 0.0, rColor, gColor, bColor);
+        var vertex2 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), 0.0, rColor, gColor, bColor);
+        var vertex3 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), 0.0, rColor, gColor, bColor);
+  
+        vertices.push(vertex1);
+        vertices.push(vertex2);
+        vertices.push(vertex3);
+  
+        return vertices;
+    }
+  }
+  
