ea3b69cc719885955931d05c75216e1f10ff2813
Isai Lopez <ilopezro@ucsc.edu>
2019-05-05 01:04:03 -0700

    added new asg3 shader

---
+++ asg3/src/graphics/shaders/asg3.js
@@ -0,0 +1,26 @@
+// Vertex Shader
+var ASG3_VSHADER =
+  `precision mediump float;
+  attribute vec4 a_Position;
+  attribute vec4 a_Color;
+  attribute vec2 a_TexCoord;
+  varying vec2 v_TexCoord;
+  varying vec4 v_Color;
+
+  void main() {
+    v_Color = a_Color;
+    v_TexCoord = a_TexCoord;
+    gl_Position = a_Position;
+  }`;
+
+// Fragment Shader
+var ASG3_FSHADER =
+  `precision mediump float;
+  varying vec4 v_Color;
+  varying vec2 v_TexCoord;
+
+  uniform sampler2D u_Sampler;
+
+  void main() {
+    gl_FragColor = texture2D(u_Sampler, v_TexCoord);
+  }`;
