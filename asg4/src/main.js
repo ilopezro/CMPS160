2da198d9bfd885df1c37c1e4e1033bafd70c0848
Isai Lopez <ilopezro@ucsc.edu>
2019-05-13 23:35:03 -0700

    cubes

--- asg4/src/main.js
+++ asg4/src/main.js
@@ -34,25 +34,33 @@ function main() {
   // Load texture and add triangle to the scene with that texture.
   inputHandler.readTexture("objs/brick.jpg", function(image) {
     for (i = 0; i < 1;i = i +.25){
-      for (j = 0; j < 1;j = j +.25){
-        var shape = new Cube(shader,[i, j-.875, 0], .125, image)
-        scene.addGeometry(shape)
-      }
-    }
+      var shape = new Cube(shader,[i, -.875, 0], .125, image)
+      scene.addGeometry(shape)
+  }
+  for (i = .25; i < 1;i = i +.25){
+      var shape = new Cube(shader,[i, -.625, 0], .125, image)
+      scene.addGeometry(shape)
+  }
+  for (i = .5; i < 1;i = i +.25){
+      var shape = new Cube(shader,[i, -.375, 0], .125, image)
+      scene.addGeometry(shape)
+  }
+  var shape = new Cube(shader,[.75, -.125, 0], .125, image)
+  scene.addGeometry(shape)
 
-    for (i = 0; i < 1;i = i +.25){
-      for (j = 0; j < 1;j = j +.25){
-        var shape = new Cube(shader,[-1, i-.875, j], .125, image)
-        scene.addGeometry(shape)
-      }
+  for (i = 0; i < 1;i = i +.25){
+    for (j = 0; j < 1;j = j +.25){
+      var shape = new Cube(shader,[-1, i-.875, j], .125, image)
+      scene.addGeometry(shape)
     }
+  }
 
-    for (i = 0; i < 1;i = i +.25){
-      for (j = 0; j < 1;j = j +.25){
-        var shape = new Cube(shader,[1, i-.875, j], .125, image)
-        scene.addGeometry(shape)
-      }
+  for (i = 0; i < 1;i = i +.25){
+    for (j = 0; j < 1;j = j +.25){
+      var shape = new Cube(shader,[1, i-.875, j], .125, image)
+      scene.addGeometry(shape)
     }
+  }
   })
 
   //create square and add it 
