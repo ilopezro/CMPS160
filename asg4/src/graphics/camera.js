85a9094eadcc60c3ea35fcb905e23b3f04696c13
Isai Lopez <ilopezro@ucsc.edu>
2019-05-13 23:12:10 -0700

    added camera perspective at the beginning

--- asg4/src/graphics/camera.js
+++ asg4/src/graphics/camera.js
@@ -34,8 +34,6 @@ class Camera {
         //variable for perspective 
         this.pers = 30; 
 
-        this.projectionMatrix.setOrtho(this.left, this.right, this.bottom, this.top, 1, 10)
-
         this.isOrtho = true; 
     }
 
@@ -175,7 +173,7 @@ class Camera {
                     this.top -= change
                 }
             }
-            this.projectionMatrix.setOrtho(this.left, this.right, this.bottom, this.top, 1, 10)
+            this.projectionMatrix.setOrtho(this.left, this.right, this.bottom, this.top, .1, 10)
         }else if(!this.isOrtho){
             if(this.pers > 5 && this.pers < 120){
                 this.pers += dir
@@ -188,7 +186,7 @@ class Camera {
                     this.pers += dir
                 }
             }
-            this.projectionMatrix.setPerspective(this.pers, canvas.width/canvas.height, 1, 100)
+            this.projectionMatrix.setPerspective(this.pers, canvas.width/canvas.height, .1, 10)
         }
     }
 
@@ -197,10 +195,10 @@ class Camera {
         this.counter++
         
         if(this.counter %2 == 1){
-            this.projectionMatrix.setPerspective(this.pers, canvas.width/canvas.height, 1, 100)
+            this.projectionMatrix.setPerspective(this.pers, canvas.width/canvas.height, .1, 10)
             this.isOrtho = false; 
         }else{
-            this.projectionMatrix.setOrtho(this.left, this.right, this.bottom, this.top, 1, 10)
+            this.projectionMatrix.setOrtho(this.left, this.right, this.bottom, this.top, .1, 10)
             this.isOrtho = true; 
         }
     }
