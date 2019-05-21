5cbeae1270773c5e388bfe972f4efc8a15488b0a
Isai Lopez <ilopezro@ucsc.edu>
2019-05-21 00:18:55 -0700

    added specular

--- asg5/src/graphics/light.js
+++ asg5/src/graphics/light.js
@@ -3,9 +3,9 @@ class Light {
         this.pos = new Vector3([x, y, z]);
 
         // light colors
-        this.ambient = [0.0,0.0,0.0];
-        this.diffuse = [0.0,0.0,0.0];
-
+        this.ambient = [1.0,1.0,1.0];
+        this.diffuse = [0.5,0.5,0.5];
+        this.specular = [1.0,1.0,1.0]
         // Later you will add specular here too.
     }
 }
