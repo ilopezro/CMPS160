572ecb231b776f713329cea7f1f32a11e2005c67
Isai Lopez <ilopezro@ucsc.edu>
2019-05-28 16:51:12 -0700

    "button" working

--- finalProject/src/input.js
+++ finalProject/src/input.js
@@ -10,23 +10,72 @@ class InputHandler {
     /**
      * Initializes the event handeling functions within the program.
      */
-    constructor(canvas, scene, camera) {
+    constructor(canvas, scene, camera, hud) {
       this.canvas = canvas;
       this.scene  = scene;
       this.camera = camera;
+      this.hud = hud
+
+      this.isClicked = false; 
 
       _inputHandler = this;
 
       this.isMoving = false; 
 
+      this.stepsTaken = 0;
+      this.time = 30;
+
+      this.ctx = hud.getContext('2d');
+
+      this.ctx.beginPath()
+      this.ctx.font = "20px Arial";
+      this.ctx.fillText("You have taken " + this.stepsTaken + " steps.", 50, 50);
+      this.ctx.fillText("You have " + this.time + " seconds left.", 50, 75);
+      this.ctx.closePath()
+
+      //creates the lavendar layer at the top of the hud
+      this.ctx.beginPath()
+      this.ctx.rect(0, 0, 400, 400);
+      this.ctx.fillStyle = "lavender";
+      this.ctx.fill();
+      this.ctx.closePath()
+
+      //this is play button
+      this.ctx.beginPath()
+      this.ctx.rect(150, 300, 75, 25);
+      this.ctx.fillText("Play!!", 150, 300)
+      this.ctx.fillStyle = "red";
+      this.ctx.fill();
+      this.ctx.closePath()
+
+    this.timer = null
+
       // Mouse Events
-      this.canvas.onmousedown = function(ev) { this.isMoving = true; };
-      this.canvas.onmouseup = function() {this.isMoving = false; }
-      this.canvas.onmousemove = function(ev) {if(this.isMoving) _inputHandler.mouseMove(ev) };
-      this.canvas.addEventListener('wheel', function(ev) { _inputHandler.mouseZoom(ev)}, false)
+      this.hud.onmousedown = function(ev) { this.isMoving = true; _inputHandler.mouseClick(ev)};
+      this.hud.onmouseup = function() {this.isMoving = false; }
+      this.hud.onmousemove = function(ev) {if(this.isMoving) _inputHandler.mouseMove(ev) };
+      this.hud.addEventListener('wheel', function(ev) { _inputHandler.mouseZoom(ev)}, false)
 
       // Keyboard Events
-      document.addEventListener('keydown', function(ev) { _inputHandler.keyDown(ev); }, false);
+      document.addEventListener('keydown', function(ev) { 
+            _inputHandler.keyDown(ev); 
+        }, 
+        false);
+    }
+
+    mouseClick(ev){
+        var x = ev.clientX
+        var y = ev.clientY
+
+        if(x > 150 && x < 225 && y > 300 && y < 350){
+            this.isClicked = true
+            this.ctx.beginPath()
+            this.ctx.clearRect(0,0,400,400)
+            this.ctx.fillText("You have taken " + this.stepsTaken + " steps.", 50, 50);
+            this.ctx.fillText("You have " + this.time + " seconds left.", 50, 75);
+            this.ctx.closePath()
+            _inputHandler.startTimer()
+        }
     }
 
     mouseMove(ev) {
@@ -57,17 +106,26 @@ class InputHandler {
 
     keyDown(ev) {
         var keyName = event.key
-
-        if(keyName == "a" || keyName == "ArrowLeft") {
-            this.camera.truck(-1);
-        }else if(keyName == "d" || keyName == "ArrowRight") {
-            this.camera.truck(1);
-        }else if(keyName == "w" || keyName == "ArrowUp"){
-            this.camera.dolly(-1)
-        }else if(keyName == "s" || keyName == "ArrowDown"){
-            this.camera.dolly(1)
-        }else if(keyName =='z'){
-            this.camera.setDistance();
+        if(!this.isClicked){
+            return; 
+        }else{
+            if(keyName == "a" || keyName == "ArrowLeft") {
+                this.camera.truck(-1);
+                this.stepsTaken++
+                _inputHandler.updateStep(); 
+            }else if(keyName == "d" || keyName == "ArrowRight") {
+                this.camera.truck(1);
+                this.stepsTaken++
+                _inputHandler.updateStep(); 
+            }else if(keyName == "w" || keyName == "ArrowUp"){
+                this.camera.dolly(-1)
+                this.stepsTaken++
+                _inputHandler.updateStep(); 
+            }else if(keyName == "s" || keyName == "ArrowDown"){
+                this.camera.dolly(1)
+                this.stepsTaken++
+                _inputHandler.updateStep(); 
+            }
         }
     }
 
@@ -89,4 +147,18 @@ class InputHandler {
         image.src = src
         return true;
     }
+
+    updateStep() {
+        this.ctx.clearRect(0,0,400,400)
+        this.ctx.fillText("You have taken " + this.stepsTaken + " steps.", 50, 50);
+  
+        if (this.time >0)
+          this.ctx.fillText("You have " + this.time + " seconds left.", 50, 75);
+        else
+          this.ctx.fillText("You have lost.", 50, 75)
+      }
+
+      startTimer(){
+        this.timer = setInterval(function(){_inputHandler.time--; _inputHandler.updateStep(); }, 1000);
+      }
 }
