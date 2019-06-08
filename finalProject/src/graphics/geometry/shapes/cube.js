1e89f344a080b749029f88166fbc75ef56e73d00
Isai Lopez <ilopezro@ucsc.edu>
2019-06-07 17:01:40 -0700

    modified cube and square to allow for color vs textured

--- finalProject/src/graphics/geometry/shapes/cube.js
+++ finalProject/src/graphics/geometry/shapes/cube.js
@@ -14,34 +14,147 @@ class Cube extends Geometry {
      * @param size Size of the cube
      * @returns {Cube} Circle created
      */
-    constructor(shader, g_points, height, size, image) {
+    constructor(shader, g_points, height, size, image, r, g, b) {
         super(shader);
         this.image = image
 
-        this.vertices = this.generateCubeVertices(g_points, height, size);
+        this.vertices = this.generateCubeVertices(g_points, height, size, r, g, b);
         this.faces = {0: this.vertices};
   
         // CALL THIS AT THE END OF ANY SHAPE CONSTRUCTOR
         this.interleaveVertices();
     }
 
-    generateCubeVertices(g_points, height, size){
+    generateCubeVertices(g_points, height, size, r, g, b){
         var vertices = []
 
         //front face of cube with full texture
         var vertex1 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]);
-        vertex1.texCoord = [0.0, 0.0]
         var vertex2 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
-        vertex2.texCoord = [1.0, height]
         var vertex3 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
-        vertex3.texCoord = [0.0, height]
         var vertex4 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]);
-        vertex4.texCoord = [0.0, 0.0]
         var vertex5 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]);
-        vertex5.texCoord = [1.0, 0.0]
         var vertex6 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
-        vertex6.texCoord = [1.0, height]
+       
+
+        //top face of cube with the bottom half of the texture 
+        var vertex7  = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
+        var vertex8  = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+        var vertex9  = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+        var vertex10 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
+        var vertex11 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
+        var vertex12 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
         
+
+        //back face of cube with the two images 
+        var vertex13 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
+        var vertex14 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+        var vertex15 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+        var vertex16 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size),g_points[2] -Number(size)*2);
+        var vertex17 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
+        var vertex18 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+        
+
+        //left facing cube has the image three times in a 3x3 table
+        var vertex25 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]);
+        var vertex26 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
+        var vertex27 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+        var vertex28 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]);
+        var vertex29 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
+        var vertex30 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+        
+
+        //Right face has 4 faces on it a 2x2 table
+        var vertex31 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]);
+        var vertex32 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
+        var vertex33 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+        var vertex34 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]);
+        var vertex35 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
+        var vertex36 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
+
+        if(this.image != null){
+            //front cube
+            vertex1.texCoord = [0.0, 0.0]
+            vertex2.texCoord = [1.0, height]
+            vertex3.texCoord = [0.0, height]
+            vertex4.texCoord = [0.0, 0.0]
+            vertex5.texCoord = [1.0, 0.0]
+            vertex6.texCoord = [1.0, height]
+
+            //top cube 
+            vertex7.texCoord = [0.0, 0.0]
+            vertex8.texCoord = [1.0, 1.0]
+            vertex9.texCoord = [0.0, 1.0]
+            vertex10.texCoord = [0.0, 0.0]
+            vertex11.texCoord = [1.0, 0.0]
+            vertex12.texCoord = [1.0, 1.0]
+
+            //back cube
+            vertex13.texCoord = [0.0, 0.0]
+            vertex14.texCoord = [1.0, height]
+            vertex15.texCoord = [0.0, height]
+            vertex16.texCoord = [0.0, 0.0]
+            vertex17.texCoord = [1.0, 0.0]
+            vertex18.texCoord = [1.0, height]
+
+            //left cube
+            vertex25.texCoord = [0.0, 0.0]
+            vertex26.texCoord = [0.0, height]
+            vertex27.texCoord = [1.0, height]
+            vertex28.texCoord = [0.0, 0.0]
+            vertex29.texCoord = [1.0, 0.0]
+            vertex30.texCoord = [1.0, height]
+
+            //right cube
+            vertex31.texCoord = [0.0,0.0]
+            vertex32.texCoord = [0.0, height] 
+            vertex33.texCoord = [1.0, height]
+            vertex34.texCoord = [0.0,0.0]
+            vertex35.texCoord = [1.0, 0.0]
+            vertex36.texCoord = [1.0, height]
+        }else if(this.image == null){
+            //front cube
+            vertex1.color = [r/255, g/255, b/255, 1.0] 
+            vertex2.color = [r/255, g/255, b/255, 1.0] 
+            vertex3.color = [r/255, g/255, b/255, 1.0] 
+            vertex4.color = [r/255, g/255, b/255, 1.0] 
+            vertex5.color = [r/255, g/255, b/255, 1.0] 
+            vertex6.color = [r/255, g/255, b/255, 1.0] 
+
+            //top cube 
+            vertex7.color = [r/255, g/255, b/255, 1.0] 
+            vertex8.color = [r/255, g/255, b/255, 1.0] 
+            vertex9.color = [r/255, g/255, b/255, 1.0] 
+            vertex10.color = [r/255, g/255, b/255, 1.0] 
+            vertex11.color = [r/255, g/255, b/255, 1.0] 
+            vertex12.color = [r/255, g/255, b/255, 1.0] 
+
+            //back cube
+            vertex13.color = [r/255, g/255, b/255, 1.0] 
+            vertex14.color = [r/255, g/255, b/255, 1.0] 
+            vertex15.color = [r/255, g/255, b/255, 1.0] 
+            vertex16.color = [r/255, g/255, b/255, 1.0] 
+            vertex17.color = [r/255, g/255, b/255, 1.0] 
+            vertex18.color = [r/255, g/255, b/255, 1.0] 
+
+            //left cube
+            vertex25.color = [r/255, g/255, b/255, 1.0] 
+            vertex26.color = [r/255, g/255, b/255, 1.0] 
+            vertex27.color = [r/255, g/255, b/255, 1.0] 
+            vertex28.color = [r/255, g/255, b/255, 1.0] 
+            vertex29.color = [r/255, g/255, b/255, 1.0] 
+            vertex30.color = [r/255, g/255, b/255, 1.0] 
+
+            //right cube
+            vertex31.color = [r/255, g/255, b/255, 1.0] 
+            vertex32.color = [r/255, g/255, b/255, 1.0]  
+            vertex33.color = [r/255, g/255, b/255, 1.0] 
+            vertex34.color = [r/255, g/255, b/255, 1.0] 
+            vertex35.color = [r/255, g/255, b/255, 1.0] 
+            vertex36.color = [r/255, g/255, b/255, 1.0] 
+        }
+
+
 
         //adding front face of cube vertices
         vertices.push(vertex1);
@@ -51,20 +164,6 @@ class Cube extends Geometry {
         vertices.push(vertex5);
         vertices.push(vertex6);
 
-        //top face of cube with the bottom half of the texture 
-        var vertex7  = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
-        vertex7.texCoord = [0.0, 0.0]
-        var vertex8  = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex8.texCoord = [1.0, 1.0]
-        var vertex9  = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex9.texCoord = [0.0, 1.0]
-        var vertex10 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
-        vertex10.texCoord = [0.0, 0.0]
-        var vertex11 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
-        vertex11.texCoord = [1.0, 0.0]
-        var vertex12 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex12.texCoord = [1.0, 1.0]
-
         //adding top face of cube vertices
         vertices.push(vertex7);
         vertices.push(vertex8);
@@ -72,20 +171,6 @@ class Cube extends Geometry {
         vertices.push(vertex10);
         vertices.push(vertex11);
         vertices.push(vertex12);
-
-        //back face of cube with the two images 
-        var vertex13 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
-        vertex13.texCoord = [0.0, 0.0]
-        var vertex14 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex14.texCoord = [1.0, height]
-        var vertex15 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex15.texCoord = [0.0, height]
-        var vertex16 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size),g_points[2] -Number(size)*2);
-        vertex16.texCoord = [0.0, 0.0]
-        var vertex17 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
-        vertex17.texCoord = [1.0, 0.0]
-        var vertex18 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex18.texCoord = [1.0, height]
 
         //adding back face of cube vertices
         vertices.push(vertex13);
@@ -95,42 +180,6 @@ class Cube extends Geometry {
         vertices.push(vertex17);
         vertices.push(vertex18);
 
-        // // //bottom face of vertices with half the image on the cube
-        // // var vertex19 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]);
-        // // vertex19.texCoord = [0.0, 1.0]
-        // // var vertex20 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]);
-        // // vertex20.texCoord = [1.0, 1.0]
-        // // var vertex21 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
-        // // vertex21.texCoord = [0.0, 0.0]
-        // // var vertex22 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
-        // // vertex22.texCoord = [0.0, 0.0]
-        // // var vertex23 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]);
-        // // vertex23.texCoord = [1.0, 1.0]
-        // // var vertex24 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
-        // // vertex24.texCoord = [1.0, 0.0]
-
-        // // //bottom face of cube vertices being added
-        // // vertices.push(vertex19);
-        // // vertices.push(vertex20);
-        // // vertices.push(vertex21);
-        // // vertices.push(vertex22);
-        // // vertices.push(vertex23);
-        // // vertices.push(vertex24);
-
-        //left facing cube has the image three times in a 3x3 table
-        var vertex25 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]);
-        vertex25.texCoord = [0.0, 0.0]
-        var vertex26 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
-        vertex26.texCoord = [0.0, height]
-        var vertex27 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex27.texCoord = [1.0, height]
-        var vertex28 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]);
-        vertex28.texCoord = [0.0, 0.0]
-        var vertex29 = new Vertex(g_points[0]-Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
-        vertex29.texCoord = [1.0, 0.0]
-        var vertex30 = new Vertex(g_points[0]-Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex30.texCoord = [1.0, height]
-
         //adding left face of cube vertices 
         vertices.push(vertex25);
         vertices.push(vertex26);
@@ -138,20 +187,6 @@ class Cube extends Geometry {
         vertices.push(vertex28);
         vertices.push(vertex29);
         vertices.push(vertex30);
-
-        //Right face has 4 faces on it a 2x2 table
-        var vertex31 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]);
-        vertex31.texCoord = [0.0,0.0]
-        var vertex32 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]);
-        vertex32.texCoord = [0.0, height] 
-        var vertex33 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex33.texCoord = [1.0, height]
-        var vertex34 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]);
-        vertex34.texCoord = [0.0,0.0]
-        var vertex35 = new Vertex(g_points[0]+Number(size), g_points[1]-Number(size), g_points[2]-Number(size)*2);
-        vertex35.texCoord = [1.0, 0.0]
-        var vertex36 = new Vertex(g_points[0]+Number(size), (g_points[1]+Number(size))*-height, g_points[2]-Number(size)*2);
-        vertex36.texCoord = [1.0, height]
 
         //adding right face of vertices
         vertices.push(vertex31);
--- finalProject/src/graphics/geometry/shapes/square.js
+++ finalProject/src/graphics/geometry/shapes/square.js
@@ -12,19 +12,19 @@ class Square extends Geometry {
      * @param {Shader} shader Shading object used to shade geometry
      * @returns {Square} Square created
      */
-    constructor(shader, image) {
+    constructor(shader, image, r, g, b) {
         super(shader);
 
         this.image = image
   
-        this.vertices = this.generateSquareVertices()
+        this.vertices = this.generateSquareVertices(r, g, b)
         this.faces = {0: this.vertices};
   
         // CALL THIS AT THE END OF ANY SHAPE CONSTRUCTOR
         this.interleaveVertices();
     }
   
-    generateSquareVertices() {
+    generateSquareVertices(r, g, b) {
         var vertices = []
   
         var vertex1 = new Vertex(-16, -1.5, -16)
@@ -34,13 +34,22 @@ class Square extends Geometry {
         var vertex5 = new Vertex(-16, -1.5, 16)
         var vertex6 = new Vertex(16, -1.5, 16)
 
-        vertex1.texCoord = [0.0, 0.0]
-        vertex2.texCoord = [16.0, 0.0]
-        vertex3.texCoord = [16.0, 16.0]
-        vertex4.texCoord = [0.0, 0.0]
-        vertex5.texCoord = [0.0, 16.0]
-        vertex6.texCoord = [16.0, 16.0] 
-  
+        if(this.image != null){
+            vertex1.texCoord = [0.0, 0.0]
+            vertex2.texCoord = [16.0, 0.0]
+            vertex3.texCoord = [16.0, 16.0]
+            vertex4.texCoord = [0.0, 0.0]
+            vertex5.texCoord = [0.0, 16.0]
+            vertex6.texCoord = [16.0, 16.0] 
+        } else if(this.image == null){
+            vertex1.color = [r/255,g/255,b/255, 1.0]
+            vertex2.color = [r/255,g/255,b/255, 1.0]
+            vertex3.color = [r/255,g/255,b/255, 1.0]
+            vertex4.color = [r/255,g/255,b/255, 1.0]
+            vertex5.color = [r/255,g/255,b/255, 1.0]
+            vertex6.color = [r/255,g/255,b/255, 1.0]
+        }
+
         vertices.push(vertex1);
         vertices.push(vertex2);
         vertices.push(vertex3);
